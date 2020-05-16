99f8047a677f5006f3f42129c02480a5c59dadc3
JulianCedric <julian.pormentilla@gmail.com>
2020-05-15 20:48:38 -0400

    Done.

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,21 @@
+class CreateArtists < ActiveRecord::Migration[4.2]
+
+    def up
+        # "do"
+    end
+
+    def down
+        # "undo"
+    end
+
+    def change
+        create_table :artists do |t|
+            # this create_table method creates a table with the name of the table written as a symbol (:artists) 
+            t.string :name
+            t.string :genre
+            t.integer :age
+            t.string :hometown 
+        end 
+    end
+
+end
